85190ce7d930e98971451e197dc419924e224a1d
wpcww <60952195+wpcww@users.noreply.github.com>
2022-08-04 11:52:12 +0800

    Added delete content, fixed and Runnable

---
+++ lambda/delete.js
@@ -0,0 +1,41 @@
+var AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+    sslEnabled: false,
+    paramValidation: false,
+    convertResponseTypes: false,
+    region: 'ap-east-1'
+});
+const tableName = 'testjoblist';
+
+exports.handler = async (event, context, callback) => {
+    let responseBody = "";
+    let statusCode = 0;
+
+    const params = {
+        TableName: tableName,
+        Key:{Name: event['body']['Name']}
+    };
+
+    try {
+        console.log(event['body']['Name'])
+        const data = await docClient.delete(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 204;
+    } catch (err) {
+        responseBody = `Unable to put Product: ${err}` + " ==== " + JSON.stringify(event);
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        body: responseBody
+    };
+    
+    //console.log("Event input: " + JSON.stringify(event))
+    
+    context.succeed(response);
+
+    return event;
+};
+
