0f14d892de5b8e9e90b08dff779584e56c7ada0e
wpcww <60952195+wpcww@users.noreply.github.com>
2022-08-05 15:43:03 +0800

    Applied code formatter

--- lambda/delete.js
+++ lambda/delete.js
@@ -1,12 +1,12 @@
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
 var docClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
+    apiVersion: "2012-08-10",
     sslEnabled: false,
     paramValidation: false,
     convertResponseTypes: false,
-    region: 'ap-east-1'
+    region: "ap-east-1",
 });
-const tableName = 'testjoblist';
+const tableName = "testjoblist";
 
 exports.handler = async (event, context, callback) => {
     let responseBody = "";
@@ -14,28 +14,28 @@ exports.handler = async (event, context, callback) => {
 
     const params = {
         TableName: tableName,
-        Key:{Name: JSON.parse(event['body']).Name}
+        Key: { Name: JSON.parse(event["body"]).Name },
     };
 
     try {
-        console.log("To be deleted: " + JSON.parse(event['body']).Name)
+        console.log("To be deleted: " + JSON.parse(event["body"]).Name);
         const data = await docClient.delete(params).promise();
         responseBody = JSON.stringify(data);
         statusCode = 204;
     } catch (err) {
-        responseBody = `Unable to put Product: ${err}` + " ==== " + JSON.stringify(event);
+        responseBody =
+            `Unable to put Product: ${err}` + " ==== " + JSON.stringify(event);
         statusCode = 403;
     }
 
     const response = {
         statusCode: statusCode,
-        body: responseBody
+        body: responseBody,
     };
-    
+
     //console.log("Event input: " + JSON.stringify(event))
-    
+
     context.succeed(response);
 
     return event;
 };
-
