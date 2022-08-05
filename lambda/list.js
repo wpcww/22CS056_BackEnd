e0007668fa01022bd185e41895170db2662972c9
wpcww <60952195+wpcww@users.noreply.github.com>
2022-08-05 17:24:24 +0800

    Corrected inconsistent naming of handler

--- lambda/list.js
+++ lambda/list.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
 var docClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
+    apiVersion: "2012-08-10",
     sslEnabled: false,
     paramValidation: false,
     convertResponseTypes: false,
-    region: 'ap-east-1'
+    region: "ap-east-1",
 });
-const tableName = 'testjoblist';
+const tableName = "testjoblist";
 
-exports.hello = async (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     let params = { TableName: tableName };
     let scanResults = [];
     let items;
 
     do {
         items = await docClient.scan(params).promise();
-        console.log(items)
+        console.log(items);
         items.Items.forEach((item) => scanResults.push(item));
         params.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (typeof items.LastEvaluatedKey != "undefined");
@@ -25,12 +25,11 @@ exports.hello = async (event, context, callback) => {
     const responseBody = {
         statusCode: 200,
         body: JSON.stringify(scanResults),
-        headers:{
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    }
-    
-    return(responseBody)
-};
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+    };
 
+    return responseBody;
+};
